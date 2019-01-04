297bcbb6216c84a4fe58d32932552df89d6f74e2
“LXP” <yameteh_meepo@163.com>
2019-01-04 11:52:22 +0800

    likeguess

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -94,78 +94,75 @@
       display: none
 
     .list
-      padding: .1rem .24rem 0
-      width: 100%;
+      padding: .1rem .24rem
       box-sizing border-box
-      display: inline;
-      float: left;
       white-space: nowrap;
       overflow-x: scroll;
-      -webkit-overflow-scrolling: touch; /*解决在ios滑动不顺畅问题*/
+      -webkit-overflow-scrolling: touch;
 
-      .item + .item
-        margin-left .12rem
+    .item + .item
+      margin-left .12rem
 
-      .item
-        display: inline-block;
-        width: 2rem
+    .item
+      display: inline-block;
+      width: 2rem
 
-        &:nth-child(1) .img-wrapper
-          &:before
-            position: absolute;
-            width: .84rem
-            height: .4rem
-            content: ""
-            left: 0
-            top: -2px
-            background url("http://img1.qunarzz.com/piao/fusion/1710/ab/159673b63e6ca702.png") center center no-repeat
-            background-size 100% 100%
+      &:nth-child(1) .img-wrapper
+        &:before
+          position: absolute;
+          width: .84rem
+          height: .4rem
+          content: ""
+          left: 0
+          top: -2px
+          background url("http://img1.qunarzz.com/piao/fusion/1710/ab/159673b63e6ca702.png") center center no-repeat
+          background-size 100% 100%
 
-        &:nth-child(2) .img-wrapper
-          &:before
-            position: absolute;
-            width: .84rem
-            height: .4rem
-            content: ""
-            left: 0
-            top: -2px
-            background url("http://img1.qunarzz.com/piao/fusion/1710/2d/36d0c4adaebbbc02.png") center center no-repeat
-            background-size 100% 100%
+      &:nth-child(2) .img-wrapper
+        &:before
+          position: absolute;
+          width: .84rem
+          height: .4rem
+          content: ""
+          left: 0
+          top: -2px
+          background url("http://img1.qunarzz.com/piao/fusion/1710/2d/36d0c4adaebbbc02.png") center center no-repeat
+          background-size 100% 100%
 
-        &:nth-child(3) .img-wrapper
-          &:before
-            position: absolute;
-            width: .84rem
-            height: .4rem
-            content: ""
-            left: 0
-            top: -2px
-            background url("http://img1.qunarzz.com/piao/fusion/1710/67/edc47ffef9e96b02.png") center center no-repeat
-            background-size 100% 100%
+      &:nth-child(3) .img-wrapper
+        &:before
+          position: absolute;
+          width: .84rem
+          height: .4rem
+          content: ""
+          left: 0
+          top: -2px
+          background url("http://img1.qunarzz.com/piao/fusion/1710/67/edc47ffef9e96b02.png") center center no-repeat
+          background-size 100% 100%
 
-        .img-wrapper
-          position: relative;
+      .img-wrapper
+        position: relative;
 
 
-          img
-            width: 100%
+        img
+          width: 100%
 
-        .item-title
-          text-align center
-          font-size: .24rem
-          line-height .32rem
-          color: #212121
-          margin-top .12rem
-          ellipse()
+      .item-title
+        text-align center
+        font-size: .24rem
+        line-height .32rem
+        color: #212121
+        margin-top .12rem
+        ellipse()
 
-        .price
-          color: #616161;
-          font-size: .24rem;
-          line-height: .36rem;
-          text-align: center;
-          ellipse()
+      .price
+        color: #616161;
+        font-size: .24rem;
+        line-height: .36rem;
+        text-align: center;
+        ellipse()
 
-          span
-            color: #ff8300;
+        span
+          color: #ff8300;
 
 </style>
