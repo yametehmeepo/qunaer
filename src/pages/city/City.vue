2df6483a6b678d21e6c4dbe97549c5e32c8c89f0
“LXP” <yameteh_meepo@163.com>
2019-01-07 14:01:43 +0800

    add list better-scroll

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,18 +2,46 @@
   <div>
     <city-header></city-header>
     <city-tabbar></city-tabbar>
+    <city-list :cities="cities" :hotCities="hotCities"></city-list>
   </div>
 </template>
 
 <script>
   import CityHeader from './components/Header'
   import CityTabbar from './components/Tabbar'
+  import CityList from './components/List'
+  import axios from 'axios'
 
   export default {
     name: "City",
+    data() {
+      return {
+        cities: {},
+        hotCities: []
+      }
+    },
     components: {
       CityHeader,
-      CityTabbar
+      CityTabbar,
+      CityList
+    },
+    methods: {
+      getCityData() {
+        axios.get('/api/city.json').then(res => {
+          console.log(res)
+          if (res && res.data) {
+            this.setCityData(res.data)
+          }
+        })
+      },
+      setCityData(data) {
+        let res = data.data
+        this.cities = res.cities
+        this.hotCities = res.hotCities
+      }
+    },
+    mounted() {
+      this.getCityData()
     }
   }
 </script>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="wrapper" ref="wrapper">
+    <div>
+      <div class="citytag">
+        <div class="title">您的位置</div>
+        <div class="list-wrapper">
+          <ul class="list">
+            <li class="item" v-for="item in mylocation" :key="item.id">
+              <span class="button active">{{item.name}}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="citytag">
+        <div class="title">热门城市</div>
+        <div class="list-wrapper">
+          <ul class="list">
+            <li class="item" v-for="item in hotCities" :key="item.id">
+              <span class="button">{{item.name}}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="citytag" v-for="(value, key) in cities" :key="key">
+        <div class="title">{{key}}</div>
+        <div class="city-list-wrapper">
+          <ul class="city-list">
+            <li class="city-item" v-for="item in value" :key="item.id">
+              <span class="city-button">{{item.name}}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import BScroll from 'better-scroll'
+
+  export default {
+    name: "List",
+    props: {
+      hotCities: Array,
+      cities: Object
+    },
+    data() {
+      return {
+        mylocation: [{
+          id: '0001', name: '大连'
+        }]
+      }
+    },
+    mounted() {
+      new BScroll(this.$refs.wrapper)
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '~style/variables.styl'
+  @import '~style/mixins.styl'
+  .wrapper
+    position: absolute;
+    top: 1.52rem
+    left: 0
+    right 0
+    bottom 0
+    overflow: hidden
+
+    .citytag
+      .title
+        font-size: .24rem;
+        padding: .24rem .3rem;
+        color: #212121;
+        background-color: #f2f4f5
+
+
+      .list-wrapper
+        padding .1rem .6rem .1rem .3rem
+
+        .list
+          overflow: hidden
+          margin-left -.2rem
+
+          .item
+            float left
+            width 33.33%
+            padding-left .2rem
+            box-sizing border-box
+            margin .1rem 0
+
+            .button
+              font-size: .28rem;
+              display: block
+              border 1px solid #ccc
+              text-align center
+              line-height: .52rem;
+              border-radius 3px
+              ellipse()
+
+            .active
+              border-color $bgColor
+              color $bgColor
+
+      .city-list-wrapper
+        .city-list
+          .city-item
+            .city-button
+              display: block
+              font-size: .28rem
+              line-height: .44rem
+              padding: .2rem .6rem .2rem .3rem
+              border-bottom 1px solid #ccc
+              ellipse()
+</style>
