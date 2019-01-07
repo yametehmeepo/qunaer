a3baad8b8fa67768d11e80556cbe552f0a56f3b2
“LXP” <yameteh_meepo@163.com>
2019-01-07 11:09:57 +0800

    add tabbr

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,14 +1,19 @@
 <template>
-  <city-header></city-header>
+  <div>
+    <city-header></city-header>
+    <city-tabbar></city-tabbar>
+  </div>
 </template>
 
 <script>
   import CityHeader from './components/Header'
+  import CityTabbar from './components/Tabbar'
 
   export default {
     name: "City",
     components: {
-      CityHeader
+      CityHeader,
+      CityTabbar
     }
   }
 </script>
---
+++ src/pages/city/components/Tabbar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="wrapper">
+    <ul class="tab">
+      <li :class="{active: type===1}" @click="changeActive(1)">
+        境内
+      </li>
+      <li :class="{active: type===2}">
+        境外·港澳台
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Tabbar",
+    data() {
+      return {
+        type: 1
+      }
+    },
+    methods: {
+      changeActive(n) {
+        this.type = n
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '~style/variables.styl';
+  .wrapper
+    overflow hidden
+    background-color: $bgColor
+    padding: 0 1rem .2rem 1rem;
+
+    .tab
+      border: 1px solid #fff
+      overflow: hidden
+
+      li
+        float left
+        width: 50%
+        text-align: center
+        font-size: .28rem
+        color #fff
+        line-height: .4rem
+        touch-action: none;
+
+      .active
+        background #fff
+        color: $bgColor
+</style>
